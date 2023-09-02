d857a5d5247161e9fe0457c47b0c209045e42240
Moritz Riede <moritz@mriede.com>
2023-09-03 01:20:22 +0200

    fix: timeout issue, provide some error handling and render rainbow logo

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
-use actix_web::{web, App, HttpResponse, HttpServer, Result};
+use actix_web::{web, App, HttpResponse, HttpServer, Result, Error, dev::Service};
 use awc::Client;
 use serde_derive::{Deserialize, Serialize};
 use colored::*;
+use std::time::Duration;
 
 static LOGO: &str = "    __           _
    / /___  _____(_)     ________  ______   _____  _____
@@ -9,13 +10,49 @@ static LOGO: &str = "    __           _
  / / /_/ / /  / /_____(__  )  __/ /   | |/ /  __/ /
 /_/\\____/_/  /_/     /____/\\___/_/    |___/\\___/_/\n\n";
 
+fn rainbow_format(s: &str) -> String {
+    let colors = [
+        "red", "yellow", "green", "cyan", "blue", "magenta"
+    ];
+    let mut colored_string = String::new();
+
+    for line in s.lines() {
+        let mut colored_line = String::new();
+        for (char_index, char) in line.chars().enumerate() {
+            // This multiplication parameter defines the frequency of the rainbow. The current setting
+            // should be in the range of about `2π` to `4π` (one or two full circle rotations).
+            let color_index = (char_index as f32 * 0.2) as usize % colors.len();
+            colored_line += &char.to_string().color(colors[color_index]).to_string();
+        }
+        colored_string += &colored_line;
+        colored_string += "\n";
+    }
+
+    colored_string
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    println!("{}", LOGO.cyan().bold());
-
+    println!("{}", rainbow_format(LOGO));
     println!("{} Listening on 127.0.0.1:3000", "[server]".blue().bold());
+
     HttpServer::new(|| {
         App::new()
+            .wrap_fn(|req, srv| {
+                println!("{} Incoming request: {} {}", "[request]".green().bold(), req.method(), req.path());
+                let fut = srv.call(req);
+                async {
+                    match fut.await {
+                        Ok(res) => {
+                            Ok(res)
+                        },
+                        Err(e) => {
+                            println!("{} Error occurred: {:?}", "[error]".red().bold(), e);
+                            Err(e)
+                        }
+                    }
+                }
+            })
             .route("/api/generate", web::post().to(generate_completion))
     })
     .bind("127.0.0.1:3000")?
@@ -33,13 +70,17 @@ struct GenerateCompletionRequest {
     context: Option<Vec<i32>>,
 }
 
-async fn generate_completion(data: web::Json<GenerateCompletionRequest>) -> Result<HttpResponse> {
-    // Make a client request to the given URL
+async fn generate_completion(data: web::Json<GenerateCompletionRequest>) -> Result<HttpResponse, Error> {
+    // Make a client request to the given URL with a timeout
     let forwarded_response = Client::default()
         .post("http://localhost:11434/api/generate")
+        .timeout(Duration::from_secs(120)) // Set timeout to 2 minutes
         .send_json(&data)
         .await
-        .map_err(|e| actix_web::error::ErrorInternalServerError(e))?;
+        .map_err(|e| {
+            println!("{} Error in generate_completion: {:?}", "[error]".red().bold(), e);
+            actix_web::error::ErrorInternalServerError(e)
+        })?;
 
     // Stream the response back to the client
     Ok(HttpResponse::build(forwarded_response.status())
